5b59aa970e8e26565c57cf2d887e6e2760cbda43
Your Name <you@example.com>
2022-05-19 10:43:56 +0530

    Created Spring project concept program

---
+++ src/main/java/com/example/Springconcepts/SpringconceptsApplication.java
@@ -0,0 +1,14 @@
+package com.example.Springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconceptsApplication.class, args);
+		System.out.println("Welcome to spring program");
+	}
+
+}
